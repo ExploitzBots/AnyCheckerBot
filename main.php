a03e0b38c6aed4a849f4a44a945e69c06cbe22eb
ExploitzBots <79147710+ExploitzBots@users.noreply.github.com>
2021-06-06 17:52:10 +0530

    Update main.php

--- main.php
+++ main.php
@@ -13,28 +13,21 @@ define('API_KEY',$API_KEY);
 
 include "./Plugins/bin.php";
 include "./Plugins/iban.php";
-include "./Plugins/stripekey.php";
-include "./Plugins/weather.php";
-include "./Plugins/dictionary.php";
-include "./Plugins/proxy.php";
-include "./Plugins/covid.php";
-include "./Plugins/currency.php";
-
 
 ////////////////=========[START MESSAGE]=========////////////////
 
 if(strpos($text, "/start") === 0){
     bot('sendmessage',[
 	'chat_id'=>$chat_id,
-	'text'=>"<b>Hey $from_fname,
+	'text'=>"<b>Heya $from_fname,
 
-I'm $USERNAMEBOT. I can do several Things!😆
+I'm $USERNAMEBOT. I am Helper Of Binner!
 
-Click the Button Below to open help menu!</b>",
+Click Help Button To Use Me :</b>",
 	'parse_mode'=>'html',
 	'reply_to_message_id'=> $message_id,
 	'reply_markup'=>json_encode(['inline_keyboard'=>[
-	[['text'=>"Help 💬",'callback_data'=>"help"]],[['text'=>"Our Channel 🌐",'url'=>"https://t.me/MadPremium"]
+	[['text'=>"⚙️ Help",'callback_data'=>"help"]],[['text'=>"Our Channel 📡",'url'=>"https://t.me/EvilzBots"]
   ],'resize_keyboard'=>true])
 	
   ]);
@@ -66,12 +59,12 @@ if($data == "help"){ //Sends Help Menu if Help Button is clicked
 	'chat_id'=>$chatid,
 	'message_id'=>$messageid,
 	'text'=>"<b>Hello there! I'm $USERNAMEBOT
-I can do Several Things!</b>
+am Helper Of Binner!</b>
 
-Click on the buttons below to get documentation about specific modules!",
+I Can Check Bins & Ibans",
 	'parse_mode'=>'html',
 	'reply_markup'=>json_encode(['inline_keyboard'=>[
-	[['text'=>"Bin Checker",'callback_data'=>"bin"],['text'=>"Covid-19 Stats",'callback_data'=>"covid"],['text'=>"Currency Converter",'callback_data'=>"monigay"]],[['text'=>"Dictionary",'callback_data'=>"dict"],['text'=>"IBAN Checker",'callback_data'=>"iban"],['text'=>"Proxy Scrapper",'callback_data'=>"proxy"]],[['text'=>"SK Checker",'callback_data'=>"stripe"],['text'=>"Weather",'callback_data'=>"weather"]],
+	[['text'=>"Bin Checker",'callback_data'=>"bin"],['text'=>"IBAN Checker",'callback_data'=>"iban"]],
 	],'resize_keyboard'=>true])
 	]);
 }
@@ -83,13 +76,12 @@ if(strpos($text, "/help") === 0){ //Sends Help Menu if User sent /help.
 	bot('sendmessage',[    //Can't use "OR" Operator because it edits the Message in Callback Query and Sends Message in /help.
 	'chat_id'=>$chat_id,
 	'text'=>"<b>Hello there! I'm $USERNAMEBOT
-I can do Several Things!</b>
+I am Helper Of Binner!</b>
 
-Click on the buttons below to get documentation about specific modules!",
+I Can Check Bins & Ibans",
 	'parse_mode'=>'html',
-	'reply_to_message_id'=> $message_id,
 	'reply_markup'=>json_encode(['inline_keyboard'=>[
-	[['text'=>"Bin Checker",'callback_data'=>"bin"],['text'=>"Covid-19 Stats",'callback_data'=>"covid"],['text'=>"Currency Converter",'callback_data'=>"monigay"]],[['text'=>"Dictionary",'callback_data'=>"dict"],['text'=>"IBAN Checker",'callback_data'=>"iban"],['text'=>"Proxy Scrapper",'callback_data'=>"proxy"]],[['text'=>"SK Checker",'callback_data'=>"stripe"],['text'=>"Weather",'callback_data'=>"weather"]],
+	[['text'=>"Bin Checker",'callback_data'=>"bin"],['text'=>"IBAN Checker",'callback_data'=>"iban"]],
 	],'resize_keyboard'=>true])
 	]);
 }
@@ -115,63 +107,6 @@ Command:</b>
 
 ////////////////=========[COVID-19 STATS]=========////////////////
 
-if($data == "covid"){
-	bot('editMessageText',[
-	'chat_id'=>$chatid,
-	'message_id'=>$messageid,
-	'text'=>"<b>🌀 Covid-19 Stats 🌀
-
-Command:</b>
-
-/covid - Provides Worldwide Covid-19 Stats
-/covid <code>&lt;country&gt;</code> - Provides Covid-19 Stats for the Given Country",
-	'parse_mode'=>'html',
-	'reply_markup'=>json_encode(['inline_keyboard'=>[
-	[['text'=>"Return",'callback_data'=>"help"]],
-	],'resize_keyboard'=>true])
-	]);
-}
-
-
-////////////////=========[CURRENCY CONVERTER]=========////////////////
-
-if($data == "monigay"){
-	bot('editMessageText',[
-	'chat_id'=>$chatid,
-	'message_id'=>$messageid,
-	'text'=>"<b>🌀 Currency Converter 🌀
-
-Command:</b>
-
-/cash <code>&lt;Value&gt;&lt;from&gt;&lt;to&gt;</code> - Converts the Currency
-
-<b>Example:- /cash 20 USD INR - Converts 20 USD to INR</b>",
-	'parse_mode'=>'html',
-	'reply_markup'=>json_encode(['inline_keyboard'=>[
-	[['text'=>"Return",'callback_data'=>"help"]],
-	],'resize_keyboard'=>true])
-	]);
-}
-
-
-////////////////=========[DICTIONARY]=========////////////////
-
-if($data == "dict"){
-	bot('editMessageText',[
-	'chat_id'=>$chatid,
-	'message_id'=>$messageid,
-	'text'=>"<b>🌀 Dictionary 🌀
-
-Command:</b>
-
-/dict <code>&lt;word&gt;</code> - Provides the Meaning of the Given Word.",
-	'parse_mode'=>'html',
-	'reply_markup'=>json_encode(['inline_keyboard'=>[
-	[['text'=>"Return",'callback_data'=>"help"]],
-	],'resize_keyboard'=>true])
-	]);
-}
-
 
 ////////////////=========[IBAN CHECKER]=========////////////////
 
@@ -191,66 +126,6 @@ Command:</b>
 	]);
 }
 
-
-////////////////=========[STRIPE KEY CHECKER]=========////////////////
-
-if($data == "stripe"){
-	bot('editMessageText',[
-	'chat_id'=>$chatid,
-	'message_id'=>$messageid,
-	'text'=>"<b>🌀 SK Key Checker 🌀
-
-Command:</b>
-
-/sk <code>&lt;sk_live_xxxx&gt;</code> - Checks if the Provided Stripe API Key is Valid or Invalid.",
-	'parse_mode'=>'html',
-	'reply_markup'=>json_encode(['inline_keyboard'=>[
-	[['text'=>"Return",'callback_data'=>"help"]],
-	],'resize_keyboard'=>true])
-	]);
-}
-
-
-////////////////=========[PROXY]=========////////////////
-
-if($data == "proxy"){
-	bot('editMessageText',[
-	'chat_id'=>$chatid,
-	'message_id'=>$messageid,
-	'text'=>"<b>🌀 Proxy Scrapper 🌀
-
-Command:</b>
-
-/http - Sends Fresh HTTPs Proxies
-/socks4 - Sends Fresh Socks4 Proxies
-/socks5 - Sends Fresh Socks5 Proxies",
-	'parse_mode'=>'html',
-	'reply_markup'=>json_encode(['inline_keyboard'=>[
-	[['text'=>"Return",'callback_data'=>"help"]],
-	],'resize_keyboard'=>true])
-	]);
-}
-
-
-////////////////=========[WEATHER INFO]=========////////////////
-
-if($data == "weather"){
-	bot('editMessageText',[
-	'chat_id'=>$chatid,
-	'message_id'=>$messageid,
-	'text'=>"<b>🌀 Weather Info 🌀
-
-Command:</b>
-
-/weather <code>&lt;Name of City&gt;</code> - Provides the Current Weather of the Provided City.
-
-<b>Note:-</b> <code>It only Supports Cities.</code>",
-	'parse_mode'=>'html',
-	'reply_markup'=>json_encode(['inline_keyboard'=>[
-	[['text'=>"Return",'callback_data'=>"help"]],
-	],'resize_keyboard'=>true])
-	]);
-}
 
 
 ?>
